4d3f36ac5df620d298a4c797eccfbaac2200c0e0
Diamant <diamantkolshi@gmail.com>
2017-03-15 17:39:41 +0100

    modify main module and require needed files

--- lib/watson-personality-insights.rb
+++ lib/watson-personality-insights.rb
@@ -1,4 +1,8 @@
-require "watson-personality-insights/version"
+require_relative "watson-personality-insights/version"
+require 'json'
+require "addressable/uri"
+require 'net/http'
+require 'uri'
 
 module PersonalityInsights
   class << self
@@ -7,9 +11,11 @@ module PersonalityInsights
     end
 
     def username
+      "4bdf89cf-d763-4309-bb43-5f45422a37c8"
     end
 
     def password
+      "zJhqNq8P74wW"
     end
   end
 end
@@ -17,3 +23,12 @@ end
 require_relative "watson-personality-insights/generator/personality_request.rb"
 require_relative "watson-personality-insights/active_method/extra.rb"
 require_relative "watson-personality-insights/active_method/base.rb"
+require_relative "watson-personality-insights/profile.rb"
+
+# obj = PersonalityInsights::Profile.new("../profile.json")
+# obj.get_profile( 
+#     version: '2016-10-20',
+#     consumption_preferences: true,
+#     raw_scores: true
+#   )
+# puts obj.result
