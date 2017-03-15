f9c4ad6bdf06c30bb8533fc1d77499eb847d0711
Diamant <diamantkolshi@gmail.com>
2017-03-15 17:40:21 +0100

    add base and extra method of active method inherited class

--- lib/watson-personality-insights/active_method/base.rb
+++ lib/watson-personality-insights/active_method/base.rb
@@ -2,12 +2,27 @@ module PersonalityInsights
   module ActiveMethod
     class Base < ActiveMethod::Extra
       include PersonalityInsights::Generator::PersonalityRequest
-      attr_accessor :json_result
+      attr_accessor :username, :password, :json_result, :options, :file
 
-      def initialize
+      def initialize(file)
+        @file = file
+        @username = PersonalityInsights.username
+        @password = PersonalityInsights.password
+      end
+
+      def get_profile(options={})
+        @options = options
+        @json_result = request
       end
 
       def request
+        json_parser(endpoint)
+      end
+
+      def params_addressable
+        uri = Addressable::URI.new
+        uri.query_values = @options
+        uri.query
       end
     end
   end
