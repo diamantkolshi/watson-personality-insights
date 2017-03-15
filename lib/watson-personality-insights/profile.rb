b11b945571ce2a99ce70b736a91499a4cf9a70d7
Diamant <diamantkolshi@gmail.com>
2017-03-15 23:12:24 +0100

    add new response fields at profile model

--- lib/watson-personality-insights/profile.rb
+++ lib/watson-personality-insights/profile.rb
@@ -1,8 +1,13 @@
 module PersonalityInsights
   class Profile < ActiveMethod::Base
-  	add_response_field :word_count
-  	add_response_field :processed_language
-  	add_response_field :personality
+    add_response_field :word_count
+    add_response_field :processed_language
+    add_response_field :personality
+    add_response_field :needs
+    add_response_field :values
+    add_response_field :behavior
+    add_response_field :consumption_preferences
+    add_response_field :warnings
 
     def endpoint
       url = "v3/profile?"
