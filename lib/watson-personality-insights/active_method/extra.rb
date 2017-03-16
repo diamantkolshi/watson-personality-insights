f4215d20239aca0581a19e8818112487585d5791
Diamant <diamantkolshi@gmail.com>
2017-03-16 10:08:51 +0100

    remove binding.pry

--- lib/watson-personality-insights/active_method/extra.rb
+++ lib/watson-personality-insights/active_method/extra.rb
@@ -3,7 +3,6 @@ module PersonalityInsights
     class Extra
       def self.add_response_field(name)
         define_method(name) do
-          require 'pry';binding.pry
           @json_result[name.to_s]
         end
       end
